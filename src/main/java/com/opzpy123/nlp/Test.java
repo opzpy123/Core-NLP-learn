974c413c691f5e544b23b56e3bd3ce766e5c9ef9
赵鹏毅 <zhaopy@gantsoftware.com>
2022-08-10 16:19:25 +0800

    chg

--- src/main/java/com/opzpy123/nlp/Test.java
+++ src/main/java/com/opzpy123/nlp/Test.java
@@ -10,7 +10,6 @@ import com.opzpy123.nlp.process.SenSplit;
 
 public class Test {
     public static void main(String[] args) {
-        //sourceTest();
         String text = "清明前后，天气时雨时晴。如同人生，祸福相依。站在先人的坟前，没人是不真诚的。细想想先人们留下了什么，大多是一片茫然。就连先人的面容也大多模糊，更别谈英名了。如同飞过的鸟儿，谁能想起它们的叫声？也许那叫声当年多么与众不同，都淹没在风中雨中，淹没在无声的岁月中……五千年的历史，古今多少事，都付笑谈中。想青史留名的想法很丰满，现实却相当的骨感。还是好好活着，做点有意义的事吧，活好人立好德再说。";
         //分词
         System.out.println(String.join("\n", new Segmentation(text).getSegList()));
